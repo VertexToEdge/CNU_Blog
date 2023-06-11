0976a668b65dc8abadb1e292f6f53bb53d359cc6
VertexToEdge <vertex@g.cnu.ac.kr>
2023-06-11 14:27:59 +0900

    add basename /CNU_Blog

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/CNU_Blog">
       <Routes>
         <Route element={<Header />}>
           <Route element={<Layout />}>
